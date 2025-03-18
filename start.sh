3d4d83ec24d449f572af1b86ffbba0e5bae48ca9
Free <noname.spyware@gmail.com>
2025-03-18 11:23:47 +0700

    Support OT reason field

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 export ACTION_NAME=$1
 export FROM_DATE=$2
 export TO_DATE=$3
+export OT_REASON=${4:-"Weekly meeting"}
 export PY_SCRIPT="python scripts/ez_master.py -u duyta@larion.com"
-
-test $FROM_TIME || export FROM_TIME="21:00"
-test $TO_TIME || export TO_TIME="23:00"
+export FROM_TIME=${FROM_TIME:-"21:00"}
+export TO_TIME=${TO_TIME:-"23:00"}
 
 echo "[!] Activating python environment..."
 source venv/Scripts/activate
@@ -16,5 +16,5 @@ echo "[!] Running the script..."
 if [[ $ACTION_NAME == "WFH" ]]; then
 	$PY_SCRIPT -t WORK_FROM_HOME -fd $FROM_DATE -td $TO_DATE --reason "WFH as planned"
 elif [[ $ACTION_NAME == "OT" ]]; then
-	$PY_SCRIPT -t OVER_TIME -fd $FROM_DATE -td $TO_DATE -ft $FROM_TIME -tt $TO_TIME
+	$PY_SCRIPT -t OVER_TIME -fd $FROM_DATE -td $TO_DATE -ft $FROM_TIME -tt $TO_TIME --reason "$OT_REASON"
 fi
