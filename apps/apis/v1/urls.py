7d422c6d773526555ea997f1c5fe6d91d7fdfb42
Free <noname.spyware@gmail.com>
2025-06-17 10:24:00 +0700

    Cleanup project

--- apps/apis/v1/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-URL configuration for lpm_api project.
-"""
-from rest_framework import routers
-
-from apps.apis.v1.login import Login
-
-router = routers.DefaultRouter()
-router.register("login", Login, basename="login")
