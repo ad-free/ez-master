d714a00694697e3458d6abab790d2a5d9f53f3fd
Free <noname.spyware@gmail.com>
2025-10-06 11:41:55 +0700

    Upgrade node

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ez-master/',  // Update this to match your repository name
+  base: '/ez-master/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   }
 })
