0cb2e4d561a3931a109cdaf716d6f9f5a93dd575
Free <noname.spyware@gmail.com>
2025-10-06 11:50:43 +0700

    Fix redirect page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/ez-master/',
+  base: command === 'serve' ? '/' : '/ez-master/',
   build: {
     outDir: 'dist',
     sourcemap: true,
@@ -17,4 +17,4 @@ export default defineConfig({
       },
     },
   }
-})
+}))
